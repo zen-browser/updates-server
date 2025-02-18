3a934de2c44edf8fa30ae1b3f8b833898ee10068
Sannidhya <58990408+sannidhyaroy@users.noreply.github.com>
2025-02-19 02:42:25 +0530

    `feat`: extract whitelist binary into variable

--- 1password-integration.sh
+++ 1password-integration.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+binary_name="zen"
 config_directory="/etc/1password"
 allowed_browsers_file="$config_directory/custom_allowed_browsers"
 config_backup_directory=""
@@ -23,12 +24,16 @@ rollback() {
     exit 1
 }
 
+echo -e "Welcome, just chill and we'll configure the rest for you!\n"
+
+sleep 1
+
 # Backup
 if [ -d "$config_directory" ]; then
     config_backup_directory=$(mktemp -d /tmp/1password_config_backup_XXXXXX)
     echo "Backing up \"$config_directory\" to \"$config_backup_directory\"..."
     sudo cp -r "$config_directory/." "$config_backup_directory"
-    echo -e "\nNOTE: You can restore your previous 1Password configuration from $config_backup_directory, till your /tmp directory is cleared (usually at the next boot)"
+    echo -e "\nNOTE: You can restore your previous 1Password configuration from \"$config_backup_directory\", till your /tmp directory is cleared (usually at the next boot)"
     if [ $? -ne 0 ]; then
         echo "Failed to backup \"$config_directory\". Exiting..."
         exit 1
@@ -37,10 +42,12 @@ fi
 
 # Check directory
 if [ ! -d "$config_directory" ]; then
-    sudo mkdir -pv "$config_directory"
+    sudo mkdir -p "$config_directory"
     if [ $? -ne 0 ]; then
         echo "Failed to create config directory at \"$config_directory\"."
         rollback
+    else
+        echo "Created config directory at \"$config_directory\""
     fi
     directory_created=true
 else
@@ -49,7 +56,8 @@ fi
 
 # Check file
 if [ ! -f "$allowed_browsers_file" ]; then
-    echo "zen" | sudo tee -a "$allowed_browsers_file" > /dev/null
+    echo "Creating 1Password Custom Allowed Browsers file and adding Zen to it..."
+    echo "$binary_name" | sudo tee -a "$allowed_browsers_file" > /dev/null
     if [ $? -ne 0 ]; then
         echo "Failed to append to the custom allowed browsers file at \"$allowed_browsers_file\"."
         rollback
@@ -58,14 +66,15 @@ else
     echo "Custom Allowed Browsers file already exists!"
     
     # Check if Zen is already whitelisted or not
-    if ! grep -Fxq "zen" "$allowed_browsers_file"; then
-        echo "zen" | sudo tee -a "$allowed_browsers_file" > /dev/null
+    if ! grep -Fxq "$binary_name" "$allowed_browsers_file"; then
+        echo "Adding Zen to 1Password Custom Allowed Browsers file..."
+        echo "$binary_name" | sudo tee -a "$allowed_browsers_file" > /dev/null
         if [ $? -ne 0 ]; then
             echo "Failed to append to the custom allowed browsers file at \"$allowed_browsers_file\"."
             rollback
         fi
     else
-        echo "Zen is already set as whitelisted in the custom allowed browsers file"
+        echo "Zen is already whitelisted in the custom allowed browsers file"
         exit 0
     fi
 fi
