231aa6bd8a3b75affdac859e951b446f97706533
Sannidhya <58990408+sannidhyaroy@users.noreply.github.com>
2025-02-19 01:10:21 +0530

    `feat`: use mktemp and enhance verbosity

--- 1password-integration.sh
+++ 1password-integration.sh
@@ -2,19 +2,19 @@
 
 config_directory="/etc/1password"
 allowed_browsers_file="$config_directory/custom_allowed_browsers"
-config_backup_directory="/tmp/1password_config_backup_$(date +'%Y%m%d_%H%M%S')"
+config_backup_directory=""
 directory_created=false
 
 # Rollback
 rollback() {
     echo "Rolling back changes..."
 
-    if [ "$directory_created" = true ];then
-        sudo rm -rf "$config_directory"
-    elif [ -d "$config_backup_directory" ]; then
+    if [ "$directory_created" = true ]; then
+        sudo rm -rv "$config_directory"
+    elif [ -n "$config_backup_directory" ] && [ -d "$config_backup_directory" ]; then
         echo "Restoring backup of 1Password config directory at \"$config_directory\" from \"$config_backup_directory\""
-        sudo rm -rf "$config_directory"
-        sudo mv "$config_backup_directory" "$config_directory"
+        sudo rm -rv "$config_directory"
+        sudo mv -v "$config_backup_directory" "$config_directory"
         echo "Rollback complete..."
     else
         echo "Nothing to rollback! Exiting..."
@@ -25,8 +25,10 @@ rollback() {
 
 # Backup
 if [ -d "$config_directory" ]; then
+    config_backup_directory=$(mktemp -d /tmp/1password_config_backup_XXXXXX)
     echo "Backing up \"$config_directory\" to \"$config_backup_directory\"..."
-    sudo cp -r "$config_directory" "$config_backup_directory"
+    sudo cp -r "$config_directory/." "$config_backup_directory"
+    echo -e "\nNOTE: You can restore your previous 1Password configuration from $config_backup_directory, till your /tmp directory is cleared (usually at the next boot)"
     if [ $? -ne 0 ]; then
         echo "Failed to backup \"$config_directory\". Exiting..."
         exit 1
@@ -35,7 +37,7 @@ fi
 
 # Check directory
 if [ ! -d "$config_directory" ]; then
-    sudo mkdir -p "$config_directory"
+    sudo mkdir -pv "$config_directory"
     if [ $? -ne 0 ]; then
         echo "Failed to create config directory at \"$config_directory\"."
         rollback
@@ -64,6 +66,7 @@ else
         fi
     else
         echo "Zen is already set as whitelisted in the custom allowed browsers file"
+        exit 0
     fi
 fi
 
