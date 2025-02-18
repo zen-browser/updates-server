086df764d986eaee4d699a5585a741de28759f27
Sannidhya <58990408+sannidhyaroy@users.noreply.github.com>
2025-02-19 02:53:52 +0530

    `fix`: check sudo availability before executing

--- 1password-integration.sh
+++ 1password-integration.sh
@@ -6,16 +6,18 @@ allowed_browsers_file="$config_directory/custom_allowed_browsers"
 config_backup_directory=""
 directory_created=false
 
+available() { command -v $1 >/dev/null; }
+
 # Rollback
 rollback() {
     echo "Rolling back changes..."
 
     if [ "$directory_created" = true ]; then
-        sudo rm -rv "$config_directory"
+        $SUDO rm -rv "$config_directory"
     elif [ -n "$config_backup_directory" ] && [ -d "$config_backup_directory" ]; then
         echo "Restoring backup of 1Password config directory at \"$config_directory\" from \"$config_backup_directory\""
-        sudo rm -rv "$config_directory"
-        sudo mv -v "$config_backup_directory" "$config_directory"
+        $SUDO rm -rv "$config_directory"
+        $SUDO mv -v "$config_backup_directory" "$config_directory"
         echo "Rollback complete..."
     else
         echo "Nothing to rollback! Exiting..."
@@ -23,6 +25,15 @@ rollback() {
 
     exit 1
 }
+
+SUDO=
+if [ "$(id -u)" -ne 0 ]; then
+    if ! available sudo; then
+        echo "This script requires superuser permissions. Please re-run as root."
+        exit 1
+    fi
+    SUDO="sudo"
+fi
 
 echo -e "Welcome, just chill and we'll configure the rest for you!\n"
 
@@ -32,8 +43,8 @@ sleep 1
 if [ -d "$config_directory" ]; then
     config_backup_directory=$(mktemp -d /tmp/1password_config_backup_XXXXXX)
     echo "Backing up \"$config_directory\" to \"$config_backup_directory\"..."
-    sudo cp -r "$config_directory/." "$config_backup_directory"
-    echo -e "\nNOTE: You can restore your previous 1Password configuration from \"$config_backup_directory\", till your /tmp directory is cleared (usually at the next boot)"
+    $SUDO cp -r "$config_directory/." "$config_backup_directory"
+    echo -e "\nNOTE: You can restore your previous 1Password configuration from \"$config_backup_directory\", anytime before your /tmp directory is cleared (usually at the next boot).\n"
     if [ $? -ne 0 ]; then
         echo "Failed to backup \"$config_directory\". Exiting..."
         exit 1
@@ -42,7 +53,7 @@ fi
 
 # Check directory
 if [ ! -d "$config_directory" ]; then
-    sudo mkdir -p "$config_directory"
+    $SUDO mkdir -p "$config_directory"
     if [ $? -ne 0 ]; then
         echo "Failed to create config directory at \"$config_directory\"."
         rollback
@@ -57,7 +68,7 @@ fi
 # Check file
 if [ ! -f "$allowed_browsers_file" ]; then
     echo "Creating 1Password Custom Allowed Browsers file and adding Zen to it..."
-    echo "$binary_name" | sudo tee -a "$allowed_browsers_file" > /dev/null
+    echo "$binary_name" | $SUDO tee -a "$allowed_browsers_file" > /dev/null
     if [ $? -ne 0 ]; then
         echo "Failed to append to the custom allowed browsers file at \"$allowed_browsers_file\"."
         rollback
@@ -68,13 +79,13 @@ else
     # Check if Zen is already whitelisted or not
     if ! grep -Fxq "$binary_name" "$allowed_browsers_file"; then
         echo "Adding Zen to 1Password Custom Allowed Browsers file..."
-        echo "$binary_name" | sudo tee -a "$allowed_browsers_file" > /dev/null
+        echo "$binary_name" | $SUDO tee -a "$allowed_browsers_file" > /dev/null
         if [ $? -ne 0 ]; then
             echo "Failed to append to the custom allowed browsers file at \"$allowed_browsers_file\"."
             rollback
         fi
     else
-        echo "Zen is already whitelisted in the custom allowed browsers file"
+        echo "Zen was already whitelisted in the custom allowed browsers file. You're all set! 🐷"
         exit 0
     fi
 fi
